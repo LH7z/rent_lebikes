1d51bae8d1fb807b86008f6e09897078c9cfc60d
goncalo catarro <goncalocatarro@gmail.com>
2023-05-30 17:16:26 +0100

    mystuffEndOfDay

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :bikes, only: [:index, :show, :new, :edit, :create]
+  resources :bookings, only: [:index, :show, :new, :edit, :create]
+
   root to: "pages#home"
   devise_for :users
 end
