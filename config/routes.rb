c132573e59e7ea1cb851d1e743ac8fff10119384
luis henrique <luisham2001@gmail.com>
2023-05-31 11:04:31 +0100

    routes to booking

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :bikes, only: [:index, :show, :new, :edit, :create]
+  resources :bikes, only: [:index, :show, :new, :edit, :create] do
+    resources :booking, only: [:show, :new, :create, :edit]
+  end
   root to: "pages#home"
   devise_for :users
+  resources :bikes, only: [:destroy]
 end
