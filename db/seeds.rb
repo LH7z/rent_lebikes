d8ef38256578c2057db725a32a52ab62f1864cb1
luis henrique <luisham2001@gmail.com>
2023-06-02 11:06:32 +0100

    back end solutions

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-5.times do
-bike = Bike.new(price: "", description: "", user: user)
-bike.save
-end
+
+# 5.times do
+# bike = Bike.new(price: "", description: "", user: user)
+# bike.save
+# end
+
+user = User.new(email: "user1@test", password: "123456")
+user.save
+
+user2 = User.new(email: "user2@test", password: "123456")
+user.save
